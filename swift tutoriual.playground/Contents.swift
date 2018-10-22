e869dee3f2b993688e3da002868e82eeeb7d8af9
Emmanuel Velez Agudelo <evelez@MacBook-Pro-5.local>
2018-10-22 16:52:20 -0500

    Variables funciones

---
+++ swift tutoriual.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+//variables con var
+var string = "Hello, playground"
+//constantes con let
+let number : Int = 10
+//definicion de una funcion
+func sumar(first : Int, second: Int) ->Int{
+    return first+second
+}
+
+print(string+" "+String(sumar(first:2,second:1)))
+
+
