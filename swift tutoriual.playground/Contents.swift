ad66458a15d517df2d3e830a6e63a1446c8f7640
Emmanuel Velez Agudelo <evelez@MacBook-Pro-5.local>
2018-10-22 17:46:17 -0500

    loops conditions and castings

--- swift tutoriual.playground/Contents.swift
+++ swift tutoriual.playground/Contents.swift
@@ -3,10 +3,32 @@ import UIKit
 var string = "Hello, playground"
 //constantes con let
 let number : Int = 10
+let decimal : Double = 10.00
+
+//casting
+let castingExample = Double(number)+decimal
+print(castingExample)
 //definicion de una funcion
 func sumar(first : Int, second: Int) ->Int{
     return first+second
 }
+let interestingNumbers = [
+    "Prime": [2, 3, 5, 7, 11, 13],
+    "Fibonacci": [1, 1, 2, 3, 5, 8,13],
+    "Square": [1, 4, 9, 16, 25,36],
+]
+
+//var largest = Int.min the same than equal 0
+var largest = 0
+
+for (kind, numbers) in interestingNumbers {
+    for number in numbers {
+        if number > largest {
+            largest = number
+        }
+    }
+}
+print(largest)
 
 print(string+" "+String(sumar(first:2,second:1)))
 
