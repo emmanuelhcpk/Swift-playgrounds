8cd05eef4628cdff744f9f4a00a6ab805626d076
Emmanuel Velez Agudelo <evelez@MacBook-Pro-5.local>
2018-10-23 09:37:10 -0500

    Classes properties, and array functions

--- swift tutoriual.playground/Contents.swift
+++ swift tutoriual.playground/Contents.swift
@@ -33,3 +33,60 @@ print(largest)
 print(string+" "+String(sumar(first:2,second:1)))
 
 
+
+// clases y structs
+
+struct Estructura {
+    var nombre = ""
+    var apellido = ""
+    
+}
+class Persona {
+    var nombre = ""
+    var apellido = ""
+    func nombreCompleto() -> String {
+        return  self.nombre+" "+self.apellido
+    }
+    //computed properties
+    var completo : String{
+        get {
+            return nombre+apellido
+        }
+        set{
+        
+        }
+    }
+    // computed property solo lectura
+    var nombres : String {
+        return self.nombre + self.apellido
+    }
+}
+//algo a destacar es que para poder modificar ateibutos de la estructura tengo q definirla como una variable, en la clase no es necesario
+var estructura = Estructura()
+var estructuraInicializada = Estructura(nombre:"Emma",apellido:"Velez")
+let clase = Persona()
+clase.nombre = "Juan"
+clase.apellido = "Agudelo"
+estructura.nombre = "Pedro"
+print("nombre en la clase",clase.nombre)
+print("nombre en la struct",estructura.nombre)
+print("nombre en estructura inicializada", estructuraInicializada.nombre)
+print("nombre completo ",clase.nombreCompleto(),clase.completo)
+
+//Arrays
+var arreglo = [1,2,3,4]
+var umbral = 3
+//con map
+var menores = arreglo.map({(value:Int) -> Int in
+    if(value < umbral){
+        return value
+    }
+    return 0
+})
+print(menores)
+// con filter
+menores = arreglo.filter({
+    return $0 < umbral
+})
+
+print(menores)
